243cbd805cf4e8159b78258170be74419ab5d756
Logan Schorr <logan.schorr89@gmail.com>
2022-10-21 16:44:53 -0400

    Updating image compilation

--- DCAM560-API/DCAM560/src/example_programs/frame_capture_mapped_depth.py
+++ DCAM560-API/DCAM560/src/example_programs/frame_capture_mapped_depth.py
@@ -11,15 +11,22 @@ frameready = camera.read_frame()
 if frameready and frameready.mappedDepth:      
     frame = camera.get_frame("Depth")
     fh,fw = int(frame.height),int(frame.width)
-    save_file = "save/mappeddepth.png"
-    img_arr = np.zeros((fh,fw),dtype=int)
+    save_file_a = "save/mappeddeptha.png"
+    save_file_b = "save/mappeddepthb.png"
+    img_arr_a = np.zeros((fh,fw),dtype=int)
+    img_arr_b = np.zeros((fh,fw),dtype=int)
 
     for i in range(fh):
         for j in range(fw):
-                img_arr[i,j] = int(frame.pFrameData[3*len(img_arr[0])*i + 2*j])
+            for k in range(2):
+                if k > 0:
+                    img_arr_b[i,j] = int(frame.pFrameData[3*len(img_arr_b[0])*i + 2*j])
+                    continue
+                img_arr_a[i,j] = int(frame.pFrameData[3*len(img_arr_a[0])*i + 2*j])
 
-    cv2.imwrite(save_file,img_arr)
-    full_img = cv2.imread(save_file)
+    cv2.imwrite(save_file_a,img_arr_a)
+    cv2.imwrite(save_file_b,img_arr_b)
+    full_img = cv2.imread(save_file_a)
     lef = full_img[0:fh,0:int(fw/2)]
     rig = full_img[0:fh,int(fw/2):fw]
     blen = cv2.addWeighted(lef,.5,rig,.5,0.0)
